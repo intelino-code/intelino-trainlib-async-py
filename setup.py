30ba82c55e83f1cfd2966974ff3067b8f51f62f0
Tom Petrůšek <tom@intelino.com>
2025-10-30 00:47:31 +0100

    chore: version bump to v1.0.5

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ REQUIREMENTS = [
 
 setup(
     name="intelino-trainlib-async",
-    version="1.0.4",
+    version="1.0.5",
     description="Asynchronous Python library (SDK) for interacting with the intelino smart train.",
     long_description=long_description,
     long_description_content_type="text/markdown",
